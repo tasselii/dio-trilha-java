8a269e836ceb73e7e38dd61ab25cd76a28d40e61
tasselii <thiagotasseli@outlook.com.br>
2024-07-01 13:58:32 -0300

    final

--- AbstraindoBootcampUsandoOOemJava/src/Main.java
+++ AbstraindoBootcampUsandoOOemJava/src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -10,21 +9,57 @@ public class Main {
         Curso curso1 = new Curso();
 
         curso1.setTitulo("curso java");
-        curso1.getDescricao("descrição curso java");
+        curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
-        curso2.getDescricao("descrição curso Javascript");
+        curso2.setDescricao("descrição curso Javascript");
         curso2.setCargaHoraria(4);
+
+        Conteudo conteudo = new Curso();
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria java");
-        mentoria.getDescricao("descrição mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
-        System.out.println(curso1);
-        System.out.println(curso2);
+        /*System.out.println(mentoria);
+        //System.out.println(curso1);
+        System.out.println(curso2);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devThiago = new Dev();
+        devThiago.setNome("Thiago");
+        devThiago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        devThiago.progredir();
+        devThiago.progredir();
+        devThiago.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Thiago: " + devThiago.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Thiago: " + devThiago.getConteudosConcluidos());
+        System.out.println("XP: " + devThiago.calcularTotalXp());
+
+        System.out.println("----------");
+
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
     }
 }
--- AbstraindoBootcampUsandoOOemJava/src/br/com/dio/desafio/dominio/Curso.java
+++ AbstraindoBootcampUsandoOOemJava/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,43 +1,31 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int CargaHoraria;
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao(String descriçãoCursoJava) {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
-        return CargaHoraria;
+        return cargaHoraria;
     }
 
     public void setCargaHoraria(int cargaHoraria) {
-        CargaHoraria = cargaHoraria;
+        this.cargaHoraria = cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", CargaHoraria=" + CargaHoraria +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", CargaHoraria=" + cargaHoraria +
                 '}';
     }
 }
