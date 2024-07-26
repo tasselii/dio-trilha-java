fb8db7b24f3390e896eaad642ac3a49548b2415f
tasselii <thiagotasseli@outlook.com.br>
2024-07-26 19:24:24 -0300

    CREATE: Java Project API REST

---
+++ projeto-rest-api/src/test/java/projeto_rest_api/ProjetoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
